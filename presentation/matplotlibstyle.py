ad79ebfc206ead9f5d6fdb144f1240f6d7869252
anilyildiz <yildiz@mbd.rwth-aachen.de>
2025-05-15 14:13:49 +0200

    testing new structure for personal page

--- presentation/matplotlibstyle.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-plt.style.use('seaborn-talk')
-
-# -----CUSTOM CHANGES-----
-plt.rcParams['figure.figsize'] = 6.4, 4.8
--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ..
-eval "$(micromamba shell hook --shell bash)"
-micromamba env create -f environment.yml --name student-project
-
-
